84db45eecf13af9f16b9f5c659a3478a32e27c81
Matthew Frieri <84392475+MatthewFrieri@users.noreply.github.com>
2024-11-15 00:27:44 -0500

    Add files via upload

---
+++ drmario.asm
@@ -0,0 +1,55 @@
+################# CSC258 Assembly Final Project ###################
+# This file contains our implementation of Dr Mario.
+#
+# Student 1: Matthew Frieri, 1010328307
+#
+# We assert that the code submitted here is entirely our own 
+# creation, and will indicate otherwise when it is not.
+#
+######################## Bitmap Display Configuration ########################
+# - Unit width in pixels:       1
+# - Unit height in pixels:      1
+# - Display width in pixels:    32
+# - Display height in pixels:   32
+# - Base Address for Display:   0x10008000 ($gp)
+##############################################################################
+
+.data
+##############################################################################
+# Immutable Data
+##############################################################################
+# The address of the bitmap display. Don't forget to connect it!
+ADDR_DSPL: .word 0x10008000
+# The address of the keyboard. Don't forget to connect it!
+ADDR_KBRD: .word 0xffff0000
+
+##############################################################################
+# Mutable Data
+##############################################################################
+
+##############################################################################
+# Code
+##############################################################################
+.text
+.globl main
+    
+draw_horz_line:
+    
+
+
+draw_vert_line:
+    
+# Run the game.
+main:
+    # Initialize the game
+
+game_loop:
+    # 1a. Check if key has been pressed
+    # 1b. Check which key has been pressed
+    # 2a. Check for collisions
+	# 2b. Update locations (capsules)
+	# 3. Draw the screen
+	# 4. Sleep
+
+    # 5. Go back to Step 1
+    j game_loop
