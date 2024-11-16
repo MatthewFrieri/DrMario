74a7ee5316c54fb47c37666a8e3b1f9833c5f282
Matthew Frieri <84392475+MatthewFrieri@users.noreply.github.com>
2024-11-15 20:04:50 -0500

    Update drmario.asm

--- drmario.asm
+++ drmario.asm
@@ -33,14 +33,64 @@ ADDR_KBRD: .word 0xffff0000
 .text
 .globl main
     
-draw_horz_line:
+
     
+draw_horz_line:
+# $a0 = X pos
+# $a1 = Y pos
+# $a2 = color
+# $a3 = length
+
+    beq $a3 $zero main      # Break when done
+    jal draw_pixel          # Draw a pixel
+    addi $a0 $a0 1          # Increment the X pos
+    addi $a3 $a3 -1         # Decrease the length by 1
+    
+    j draw_horz_line        # Back to top
 
 
 draw_vert_line:
+# $a0 = X pos
+# $a1 = Y pos
+# $a2 = color
+# $a3 = length
+
+    beq $a3 $zero main      # Break when done
+    jal draw_pixel          # Draw a pixel
+    addi $a1 $a1 1          # Increment the Y pos
+    addi $a3 $a3 -1         # Decrease the length by 1
+    
+    j draw_vert_line        # Back to top
+
+
+draw_pixel:
+# $a0 = X pos
+# $a1 = Y pos
+# $a2 = color
+# $v0 = return address
+
+    lw $t0 ADDR_DSPL    # Load the root display address
+    
+    sll $t1 $a0 2       # Multiply the X pos by 4 (shift left 2)
+    add $t0 $t0 $t1     # Add the X pos to the root
+    
+    sll $t1 $a1 2       # Multiply the Y pos by 4 (shift left 2)
+    sll $t1 $t1 5       # Multiply the Y pos by 32 (shift left 5)
+    add $t0 $t0 $t1     # Add the Y pos to the root
+    
+    sw $a2 0($t0)       # Draw the pixel
+    jr $ra              # Return to function call
+    
     
 # Run the game.
 main:
+    li $a0 2
+    li $a1 2
+    li $a2 0xff0000
+    li $a3 12
+    
+    j draw_vert_line
+    # j draw_horz_line
     # Initialize the game
 
 game_loop:
